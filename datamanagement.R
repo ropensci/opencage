210a3515199c2bdcee8fac48324e6ee79a6877fb
dpprdan <1423562+dpprdan@users.noreply.github.com>
2018-01-08 17:02:36 +0100

    Fix #24, Namibia country code, limit NAs to empty strings

--- datamanagement.R
+++ datamanagement.R
@@ -1,7 +1,7 @@
-countrycodes <- readr::read_csv("inst/extdata/countrycodes.csv")
+countrycodes <- readr::read_csv("inst/extdata/countrycodes.csv", na = "")
 save(countrycodes, file = "data/countrycodes.RData")
 
-languagecodes <- readr::read_csv("inst/extdata/languagecodes.csv")
+languagecodes <- readr::read_csv("inst/extdata/languagecodes.csv", na = "")
 save(languagecodes, file = "data/languagecodes.RData")
 
 code_message <- readr::read_csv("inst/extdata/messages.csv")
