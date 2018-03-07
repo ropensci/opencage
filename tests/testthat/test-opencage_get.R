dd1f51945b5ae78545e3d6ab68aa86a5d9e8abfd
Daniel Possenriede <possenriede@gmail.com>
2018-03-07 09:33:09 +0100

    fix tests regarding endpoint in oc_build_url

--- tests/testthat/test-opencage_get.R
+++ tests/testthat/test-opencage_get.R
@@ -3,12 +3,15 @@ context("oc_get")
 test_that("oc_get returns a response object", {
   skip_on_cran()
   expect_is(
-    oc_get(oc_build_url(
-      list(
-        placename = "Sarzeau",
-        key = Sys.getenv("OPENCAGE_KEY")
+    oc_get(
+      oc_build_url(
+        query_par = list(
+          placename = "Sarzeau",
+          key = Sys.getenv("OPENCAGE_KEY")
+        ),
+        endpoint = "json"
       )
-    )),
+    ),
     "HttpResponse"
   )
 })
@@ -16,13 +19,16 @@ test_that("oc_get returns a response object", {
 test_that("oc_get returns a response object for Namibia NA countrycode", {
   skip_on_cran()
   expect_is(
-    oc_get(oc_build_url(
-      list(
-        placename = "Windhoek",
-        key = Sys.getenv("OPENCAGE_KEY"),
-        countrycode = "NA"
+    oc_get(
+      oc_build_url(
+        query_par = list(
+          placename = "Windhoek",
+          key = Sys.getenv("OPENCAGE_KEY"),
+          countrycode = "NA"
+        ),
+        endpoint = "json"
       )
-    )),
+    ),
     "HttpResponse"
   )
 })
@@ -30,11 +36,16 @@ test_that("oc_get returns a response object for Namibia NA countrycode", {
 test_that("oc_get returns a response object for vector countrycode", {
   skip_on_cran()
   expect_is(
-    oc_get(oc_build_url(list(
-      placename = "Paris",
-      key = Sys.getenv("OPENCAGE_KEY"),
-      countrycode = c("FR", "US")
-    ))),
+    oc_get(
+      oc_build_url(
+        query_par = list(
+          placename = "Paris",
+          key = Sys.getenv("OPENCAGE_KEY"),
+          countrycode = c("FR", "US")
+        ),
+        endpoint = "json"
+      )
+    ),
     "HttpResponse"
   )
 })
--- tests/testthat/test-opencage_url.R
+++ tests/testthat/test-opencage_url.R
@@ -1,5 +1,11 @@
 library("opencage")
 context("oc_build_url")
 test_that("oc_build_url returns a string", {
-  expect_is(oc_build_url(query_par = list(placename = "Haarlem")), "character")
-})
+  expect_is(
+    oc_build_url(
+      query_par = list(placename = "Haarlem"),
+      endpoint = "json"
+    ),
+    "character")
+  }
+)
