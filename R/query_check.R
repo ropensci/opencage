fff3af6d336909651ae5c7439273cd785cba2b76
Daniel Possenriede <possenriede@gmail.com>
2018-04-11 23:51:42 +0200

    vectorise query_check with pwalk

--- R/query_check.R
+++ R/query_check.R
@@ -33,9 +33,9 @@
 
 oc_query_check <-
   function(
+    placename = NULL,
     latitude = NULL,
     longitude = NULL,
-    placename = NULL,
     key = NULL,
     bounds = NULL,
     countrycode = NULL,
@@ -48,126 +48,164 @@ oc_query_check <-
     abbrv = NULL,
     add_request = NULL
   ) {
+    # vectorise
+    if (length(placename) > 1 ||
+      length(latitude) > 1 ||
+      length(longitude) > 1) {
+      arglist <-
+        purrr::compact(
+          list(
+            placename = placename,
+            latitude = latitude,
+            longitude = longitude,
+            key = key,
+            bounds = bounds,
+            countrycode = countrycode,
+            language = language,
+            limit = limit,
+            min_confidence = min_confidence,
+            no_annotations = no_annotations,
+            no_dedupe = no_dedupe,
+            no_record = no_record,
+            abbrv = abbrv,
+            add_request = add_request
+          )
+        )
+      return(
+        purrr::pwalk(
+          .l = arglist,
+          .f = oc_query_check
+        )
+      )
+    }
+
     # check latitude
-  if (!is.null(latitude)) {
-    if (!all(dplyr::between(latitude, -90, 90))) {
-      stop(call. = FALSE, "Latitude should be between -90 and 90.")
+    if (!is.null(latitude)) {
+      if (!(dplyr::between(latitude, -90, 90))) {
+        stop(call. = FALSE, "Latitude should be between -90 and 90.")
+      }
     }
-  }
 
-  # check longitude
-  if (!is.null(longitude)) {
-    if (!all(dplyr::between(longitude, -180, 180))) {
-      stop(call. = FALSE, "Longitude should be between -180 and 180.")
+    # check longitude
+    if (!is.null(longitude)) {
+      if (!(dplyr::between(longitude, -180, 180))) {
+        stop(call. = FALSE, "Longitude should be between -180 and 180.")
+      }
     }
-  }
 
-  # check placename
-  if (!is.null(placename)) {
-    if (!is.character(placename)) {
-      stop(call. = FALSE, "Placename should be a character.")
+    # check placename
+    if (!is.null(placename)) {
+      if (!is.character(placename)) {
+        stop(call. = FALSE, "Placename should be a character.")
+      }
     }
-  }
 
-  # check key
-  if (is.null(key)) {
-    stop(call. = FALSE, "A `key` must be provided.")
-  } else {
-    if (!is.character(key)) {
-      stop(call. = FALSE, "Key should be a character.")
+    # check key
+    if (is.null(key)) {
+      stop(call. = FALSE, "A `key` must be provided.")
+    } else {
+      if (!is.character(key)) {
+        stop(call. = FALSE, "Key should be a character.")
+      }
     }
-  }
 
-  # check bounds
-  if (!is.null(bounds)) {
-    if (length(bounds) != 4) {
-      stop(call. = FALSE, "bounds should be a vector of 4 numeric values.")
+    # check bounds
+    if (!is.null(bounds)) {
+      if (length(bounds) != 4) {
+        stop(call. = FALSE, "bounds should be a vector of 4 numeric values.")
+      }
+      if (!dplyr::between(bounds[1], -180, 180)) {
+        stop(call. = FALSE, "min long should be between -180 and 180.")
+      }
+      if (!dplyr::between(bounds[2], -90, 90)) {
+        stop(call. = FALSE, "min lat should be between -90 and 90.")
+      }
+      if (!dplyr::between(bounds[3], -180, 180)) {
+        stop(call. = FALSE, "max long should be between -180 and 180.")
+      }
+      if (!dplyr::between(bounds[4], -90, 90)) {
+        stop(call. = FALSE, "max lat should be between -90 and 90.")
+      }
+      if (bounds[1] > bounds[3]) {
+        stop(call. = FALSE, "min long has to be smaller than max long")
+      }
+      if (bounds[2] > bounds[4]) {
+        stop(call. = FALSE, "min lat has to be smaller than max lat")
+      }
     }
-    if (!dplyr::between(bounds[1], -180, 180)) {
-      stop(call. = FALSE, "min long should be between -180 and 180.")
-    }
-    if (!dplyr::between(bounds[2], -90, 90)) {
-      stop(call. = FALSE, "min lat should be between -90 and 90.")
-    }
-    if (!dplyr::between(bounds[3], -180, 180)) {
-      stop(call. = FALSE, "max long should be between -180 and 180.")
-    }
-    if (!dplyr::between(bounds[4], -90, 90)) {
-      stop(call. = FALSE, "max lat should be between -90 and 90.")
-    }
-    if (bounds[1] > bounds[3]) {
-      stop(call. = FALSE, "min long has to be smaller than max long")
-    }
-    if (bounds[2] > bounds[4]) {
-      stop(call. = FALSE, "min lat has to be smaller than max lat")
-    }
-  }
 
-  # check countrycode
-  if (!is.null(countrycode)) {
-    if (!(all(toupper(unlist(countrycode)) %in% countrycodes$Code))) {
-      stop(call. = FALSE, "countrycode does not have a valid value.")
+    # check countrycode
+    if (!is.null(countrycode)) {
+      if (!(all(toupper(unlist(countrycode)) %in% countrycodes$Code))) {
+        stop(call. = FALSE, "countrycode does not have a valid value.")
+      }
     }
-  }
 
-  # check language
-  if (!is.null(language)) {
-    lang <- strsplit(language, "-")[[1]]
-    if (!(lang[1] %in% languagecodes$alpha2)) {
-      stop(call. = FALSE, "The language code is not valid.")
+    # check language
+    if (!is.null(language)) {
+      lang <- strsplit(language, "-")[[1]]
+      if (!(lang[1] %in% languagecodes$alpha2)) {
+        stop(call. = FALSE, "The language code is not valid.")
+      }
+      if (length(lang) > 1) {
+        if (!(lang[2] %in% countrycodes$Code)) {
+          stop(call. = FALSE, "The country part of language is not valid.")
+        }
+      }
     }
-    if (length(lang) > 1) {
-      if (!(lang[2] %in% countrycodes$Code)) {
-        stop(call. = FALSE, "The country part of language is not valid.")
+
+    # check limit
+    if (!is.null(limit)) {
+      if (!(limit %in% c(1:100))) {
+        stop(
+          call. = FALSE,
+          "limit should be an integer between 1 and 100."
+        ) # nolint
+      }
+    }
+
+    # check min_confidence
+    if (!is.null(min_confidence)) {
+      if (!(min_confidence %in% c(1:10))) {
+        stop(
+          call. = FALSE,
+          "min_confidence should be an integer between 1 and 10."
+        )
+      }
+    }
+
+    # check abbrv
+    if (!is.null(abbrv)) {
+      if (!is.logical(abbrv)) {
+        stop(call. = FALSE, "abbrv has to be a logical.")
+      }
+    }
+
+    # check no_annotations
+    if (!is.null(no_annotations)) {
+      if (!is.logical(no_annotations)) {
+        stop(call. = FALSE, "no_annotations has to be a logical.")
+      }
+    }
+
+    # check no_record
+    if (!is.null(no_record)) {
+      if (!is.logical(no_record)) {
+        stop(call. = FALSE, "no_record has to be a logical.")
+      }
+    }
+
+    # check no_dedupe
+    if (!is.null(no_dedupe)) {
+      if (!is.logical(no_dedupe)) {
+        stop(call. = FALSE, "no_dedupe has to be a logical.")
+      }
+    }
+
+    # check add_request
+    if (!is.null(add_request)) {
+      if (!is.logical(add_request)) {
+        stop(call. = FALSE, "add_request has to be a logical.")
       }
     }
   }
-
-  # check limit
-  if (!is.null(limit)) {
-    if (!(limit %in% c(1:100))) {
-      stop(call. = FALSE, "limit should be an integer between 1 and 100.") #nolint
-    }
-  }
-
-  # check min_confidence
-  if (!is.null(min_confidence)) {
-    if (!(min_confidence %in% c(1:10))) {
-      stop(call. = FALSE, "min_confidence should be an integer between 1 and 10.") # nolint
-    }
-  }
-
-  # check abbrv
-  if (!is.null(abbrv)) {
-    if (!is.logical(abbrv)) {
-      stop(call. = FALSE, "abbrv has to be a logical.")
-    }
-  }
-  # check no_annotations
-  if (!is.null(no_annotations)) {
-    if (!is.logical(no_annotations)) {
-      stop(call. = FALSE, "no_annotations has to be a logical.")
-    }
-  }
-
-  # check no_record
-  if (!is.null(no_record)) {
-    if (!is.logical(no_record)) {
-      stop(call. = FALSE, "no_record has to be a logical.")
-    }
-  }
-
-  # check no_dedupe
-  if (!is.null(no_dedupe)) {
-    if (!is.logical(no_dedupe)) {
-      stop(call. = FALSE, "no_dedupe has to be a logical.")
-    }
-  }
-
-  # check add_request
-  if (!is.null(add_request)) {
-    if (!is.logical(add_request)) {
-      stop(call. = FALSE, "add_request has to be a logical.")
-    }
-  }
-}
