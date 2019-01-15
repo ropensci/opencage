e7ab79311e4f4c641c2bf85c900d7aeed422c21c
Daniel Possenriede <possenriede@gmail.com>
2019-01-15 08:31:48 +0100

    vcr oc_get

--- tests/testthat/test-oc_get.R
+++ tests/testthat/test-oc_get.R
@@ -1,59 +1,61 @@
 ## Test oc_get ##
 
 test_that("oc_get returns a response object", {
-  skip_if_oc_offline()
-  withr::local_envvar(c("OPENCAGE_KEY" = key_200))
+  vcr::use_cassette("oc_get_response", {
+    withr::local_envvar(c("OPENCAGE_KEY" = key_200))
 
-  expect_s3_class(
-    oc_get(
-      oc_build_url(
-        query_par = list(
-          placename = "irrelevant",
-          key = Sys.getenv("OPENCAGE_KEY")
-        ),
-        endpoint = "json"
-      )
-    ),
-    "HttpResponse"
-  )
+    expect_s3_class(
+      oc_get(
+        oc_build_url(
+          query_par = list(
+            placename = "irrelevant",
+            key = Sys.getenv("OPENCAGE_KEY")
+          ),
+          endpoint = "json"
+        )
+      ),
+      "HttpResponse"
+    )
+  })
 })
 
 test_that("oc_get returns a response object for Namibia NA countrycode", {
-  skip_if_no_key()
-  skip_if_oc_offline()
+  vcr::use_cassette("oc_get_namibia", {
 
-  expect_s3_class(
-    oc_get(
-      oc_build_url(
-        query_par = list(
-          placename = "Windhoek",
-          key = Sys.getenv("OPENCAGE_KEY"),
-          countrycode = "NA"
-        ),
-        endpoint = "json"
-      )
-    ),
-    "HttpResponse"
-  )
+    expect_s3_class(
+      oc_get(
+        oc_build_url(
+          query_par = list(
+            placename = "Windhoek",
+            key = Sys.getenv("OPENCAGE_KEY"),
+            countrycode = "NA"
+          ),
+          endpoint = "json"
+        )
+      ),
+      "HttpResponse"
+    )
+  })
 })
 
 test_that("oc_get returns a response object for vector countrycode", {
-  skip_if_no_key()
-  skip_if_oc_offline()
+  vcr::use_cassette("oc_get_countrycode", {
 
-  expect_s3_class(
-    oc_get(
-      oc_build_url(
-        query_par = list(
-          placename = "Paris",
-          key = Sys.getenv("OPENCAGE_KEY"),
-          countrycode = c("FR", "US")
-        ),
-        endpoint = "json"
-      )
-    ),
-    "HttpResponse"
-  )
+    expect_s3_class(
+      oc_get(
+        oc_build_url(
+          query_par = list(
+            placename = "Paris",
+            key = Sys.getenv("OPENCAGE_KEY"),
+            countrycode = c("FR", "US")
+          ),
+          endpoint = "json"
+        )
+      ),
+      "HttpResponse"
+    )
+
+  })
 })
 
 test_that("oc_get_limited is rate limited", {
