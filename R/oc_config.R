58b1a1615b0ff608025e1efe9397ca45c2b04036
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-05-07 22:56:42 +0200

    up-to-date pricing information
    Co-Authored-By: dpprdan <possenriede@gmail.com>

--- R/oc_config.R
+++ R/oc_config.R
@@ -10,7 +10,7 @@
 #' @details The rate limit allowed by the API depends on the OpenCage plan you
 #'   purchased and ranges from 1 request/sec for the "Free Trial" plan to 15
 #'   requests/sec for the "Medium" or "Large plan, see
-#'   \url{https://opencagedata.com/pricing} for details. You can set the rate
+#'   \url{https://opencagedata.com/pricing} for details and up-to-date information. You can set the rate
 #'   limit persistently across sessions by setting the \code{oc_rate_sec}
 #'   \link[base:options]{option} in your \code{\link[base:Startup]{.Rprofile}}.
 #'
