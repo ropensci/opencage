8d72d9dbe2aaefc1d9f3d1d96604c4e27fc8a73d
Daniel Possenriede <possenriede@gmail.com>
2019-05-08 23:59:36 +0200

    update http error messages, fixes #69

--- datamanagement.R
+++ datamanagement.R
@@ -4,5 +4,14 @@ save(countrycodes, file = "data/countrycodes.RData")
 languagecodes <- readr::read_csv("inst/extdata/languagecodes.csv", na = "")
 save(languagecodes, file = "data/languagecodes.RData")
 
-code_message <- readr::read_csv("inst/extdata/messages.csv")
+# From https://opencagedata.com/api#codes
+code_message <-
+  readr::read_csv(
+    "inst/extdata/messages.csv",
+    col_types =
+      readr::cols(
+        code = readr::col_integer(),
+        message = readr::col_character()
+      )
+    )
 save(code_message, file = "data/code_message.RData")
