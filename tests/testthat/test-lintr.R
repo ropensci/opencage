6b8ebfba60d1b3d6c3e654d2e76577081803139b
Daniel Possenriede <possenriede@gmail.com>
2019-05-20 00:28:24 +0200

    lint on travis after_success, remove lintr from tests and Suggests

--- tests/testthat/test-lintr.R
+++
@@ -1,8 +0,0 @@
-library("opencage")
-context("lintr")
-if (requireNamespace("lintr", quietly = TRUE)) {
-  context("lints")
-  test_that("Package Code Style", {
-    lintr::expect_lint_free()
-  })
-}
