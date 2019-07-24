3123b0721b5b1d81ac0ab5e727a355f04fa8a6ab
Daniel Possenriede <possenriede@gmail.com>
2019-07-25 00:13:09 +0200

    skip_if_no_key and match on uri only

--- tests/testthat/setup-opencage.R
+++ tests/testthat/setup-opencage.R
@@ -4,14 +4,18 @@ key_200 <- "6d0e711d72d74daeb2b0bfd2a5cdfdba" # always returns a 200 response
 key_402 <- "4372eff77b8343cebfc843eb4da4ddc4" # always returns a 402 responce
 key_403 <- "2e10e5e828262eb243ec0b54681d699a" # always returns a 403 responce
 key_429 <- "d6d0f0065f4348a4bdfe4587ba02714b" # always returns a 429 responce
+
+# setup vcrs
 library("vcr")
 invisible(
   vcr::vcr_configure(
     dir = "../vcr_cassettes",
+    match_requests_on = "uri", # method not necessary, we only make GET requests
     filter_sensitive_data =
       list("<<<oc_api_key>>>" = Sys.getenv("OPENCAGE_KEY"))
   )
 )
+
 # skip if API offline
 skip_if_oc_offline <- function(host = "api.opencagedata.com") {
   testthat::skip_if_offline(host = host)
