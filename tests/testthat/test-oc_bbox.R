606fd8a0379fff3e6d7c58e3784a52f0da0fcd93
Daniel Possenriede <possenriede@gmail.com>
2019-08-06 22:11:25 +0200

    move oc_check_bbox tests to test-oc_bbox.R

--- tests/testthat/test-oc_bbox.R
+++ tests/testthat/test-oc_bbox.R
@@ -82,3 +82,39 @@ test_that("oc_bbox.default gives informative error message", {
   )
 })
 
+# Test checks for oc_bbox -------------------------------------------------
+
+test_that("oc_bbox checks bbox", {
+  expect_error(
+    oc_bbox(NA_real_, 51.280430, 0.278970, 51.683979),
+    "Every `bbox` element must be non-missing."
+  )
+  expect_error(
+    oc_bbox(-0.563160, "51.280430", 0.278970, 51.683979),
+    "Every `bbox` must be a numeric vector."
+  )
+  expect_error(
+    oc_bbox(-563160, 51.280430, 0.278970, 51.683979),
+    "`xmin` must be between -180 and 180."
+  )
+  expect_error(
+    oc_bbox(-0.563160, 51280430, 0.278970, 51.683979),
+    "`ymin` must be between -90 and 90."
+  )
+  expect_error(
+    oc_bbox(-0.563160, 51.280430, 278970, 51.683979),
+    "`xmax` must be between -180 and 180."
+  )
+  expect_error(
+    oc_bbox(-0.563160, 51.280430, 0.278970, 51683979),
+    "`ymax` must be between -90 and 90."
+  )
+  expect_error(
+    oc_bbox(0.563160, 51.280430, 0.278970, 51.683979),
+    "`xmin` must always be smaller than `xmax`"
+  )
+  expect_error(
+    oc_bbox(-0.563160, 53.280430, 0.278970, 51.683979),
+    "`ymin` must always be smaller than `ymax`"
+  )
+})
