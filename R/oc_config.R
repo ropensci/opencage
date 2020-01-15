a9a492bc85f73b273f12de1de7f21110e593b4da
Daniel Possenriede <possenriede@gmail.com>
2020-01-15 23:01:22 +0100

    rewrite oc_config docs

--- R/oc_config.R
+++ R/oc_config.R
@@ -1,35 +1,44 @@
 #' Configure settings
 #'
-#' Configures session settings for \pkg{opencage}.
+#' Configure session settings for \pkg{opencage}.
 #'
 #' @param key Your OpenCage API key as a character vector of length one. Do not
-#'   enter the key directly, though. See details.
-#' @param rate_sec \code{numeric(1)} Sets the maximum number of requests sent to
-#'   the OpenCage API per second. Defaults to the value set in the
-#'   \code{oc_rate_sec} option, or, in case that does not exist, to 1.
+#'   pass the key directly as a parameter, though. See details.
+#' @param rate_sec Numeric vector of length one. Sets the maximum number of
+#'   requests sent to the OpenCage API per second. Defaults to the value set in
+#'   the \code{oc_rate_sec} option, or, in case that does not exist, to 1L.
 #' @param ... Ignored.
 #'
-#' @section Configure your OpenCage API key:
+#' @section Set your OpenCage API key:
 #'
-#' opencage uses the environment variable \code{OPENCAGE_KEY} internally to
-#' retrieve your OpenCage key. \code{\link{oc_config}} will help to set that
-#' environment variable. Do not set the key directly, though, as you risk
-#' exposing the key via your skript or your history. Instead, if set to
-#' \code{NULL}, the default, \code{\link{oc_config}} will check if the
-#' environment variable \code{OPENCAGE_KEY} is already set, and prompt you to
-#' enter the key in the console if it is not.
+#' \pkg{opencage} uses the environment variable \code{OPENCAGE_KEY} internally
+#' to retrieve your OpenCage key. \code{\link{oc_config}} will help to set that
+#' environment variable. Do not pass the key directly as a parameter to the
+#' function, though, as you risk exposing it via your skript or your history.
+#' There are three safer ways to set your API key instead:
 #'
-#' If you use a package like keyring, you can safely set your key in a script
-#' like this \code{oc_config(key = keyring::key_get("opencage"))}.
-#'
-#' Finally you can save your API key directly as an environment variable in
-#' `.Renviron` as described in
-#' \href{https://happygitwithr.com/github-pat.html#step-by-step}{Happy Git and
-#' GitHub for the useR} or
+# nolint start - link longer than 80 chars
+#' 1. Save your API key as an environment variable in
+#' \code{\link[base:Startup]{.Renviron}} as described in
+#' \href{https://rstats.wtf/r-startup.html#renviron}{What They Forgot to Teach
+#' You About R} or
 #' \href{https://csgillespie.github.io/efficientR/set-up.html#renviron}{Efficient
-#' R Programming} and skip \code{oc_config} for setting your API key altogether.
+#' R Programming}. From there it will be fetched by all functions that call the
+#' OpenCage API, so you do not even have to call \code{oc_config} to set your
+#' key, but can start geocoding right away. If you have the \pkg{usethis}
+#' package installed, you can edit your \code{\link[base:Startup]{.Renviron}}
+#' most easily with \code{usethis::edit_r_environ()}.
+# nolint end
 #'
-#' @section Configure your OpenCage API rate limit:
+#' 2. If you use a package like \pkg{keyring} to store your credentials, you can
+#' safely pass your key in a script with a function call like this
+#' \code{oc_config(key = keyring::key_get("opencage"))}.
+#'
+#' 3. If you call \code{oc_config} in an \code{\link[base]{interactive}} session and the
+#' \code{OPENCAGE_KEY} environment variable is not set, it will prompt you to
+#' enter the key in the console.
+#'
+#' @section Set your OpenCage API rate limit:
 #'
 #' The rate limit allowed by the API depends on the OpenCage plan you
 #' purchased and ranges from 1 request/sec for the "Free Trial" plan to 15
@@ -51,7 +60,7 @@ oc_config <-
     key_needed <-
       c(
         "Using the OpenCage Geocoder requires a valid API key.\n",
-        "See <https://opencagedata.com/api#forward>\n",
+        "See <https://opencagedata.com/api#forward> and help(oc_config)\n",
         "\n"
       )
 
@@ -77,4 +86,7 @@ oc_config <-
       oc_get_limited,
       ratelimitr::rate(n = rate_sec, period = 1L)
     )
+
+    # set no_record
+    options("oc_no_record" = no_record)
   }
