29fe779f070f79b5c710e2fd74339ed1c4ae1b00
charliejhadley <charliejhadley@gmail.com>
2020-06-23 13:05:46 +0100

    Insertion of query is no longer conditional on old argument

--- R/utils.R
+++ R/utils.R
@@ -38,13 +38,10 @@ oc_format <- function(res_text, return, query) {
       results <-
         rlang::set_names(results, ~ tolower(paste0("oc_", .)))
     }
-    if ("request" %in% names(jsn)) {
-      # add request directly, not from OpenCage roundtrip
-      if (identical(query, "")) query <- NA_character_
-      tibble::add_column(results, oc_query = query, .after = 0)
-    } else {
-      results
-    }
+
+    if (identical(query, "")) query <- NA_character_
+    tibble::add_column(results, oc_query = query, .after = 0)
+
   } else if (return == "json_list" || return == "geojson_list") {
     jsn <- jsonlite::fromJSON(res_text, simplifyVector = FALSE)
     if (return == "geojson_list") {
