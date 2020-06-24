a7aa4668dc91c8affad23ff8b454be710a751944
charliejhadley <charliejhadley@gmail.com>
2020-06-24 13:32:37 +0100

    remove internal column duplication checks
    We can't make good decisions about how to fix duplicates, this should bne left to the user.

--- R/oc_forward.R
+++ R/oc_forward.R
@@ -426,13 +426,6 @@ oc_forward_df.data.frame <- # nolint - see lintr issue #223
 
       if (output == "short") {
 
-        duplicated_colnames <- colnames(data)[colnames(data) %in% c("oc_query", "oc_lat", "oc_lng", "oc_formatted")]
-
-        if(!identical(duplicated_colnames, character(0))){
-          message("Column names in `df` also appear in results from OpenCage. The columns returned by OpenCage have been renamed as follows: ",
-                  paste0(duplicated_colnames, sep = "1", collapse = ", "))
-        }
-
         results <-
           dplyr::select(
             results,
@@ -445,12 +438,7 @@ oc_forward_df.data.frame <- # nolint - see lintr issue #223
         results <- tidyr::nest(results, data = oc_query:ncol(results))
       } else {
 
-        duplicated_colnames <- colnames(data)[colnames(data) %in% colnames(results)]
 
-        if(!identical(duplicated_colnames, character(0))){
-          message("Column names in `df` also appear in results from OpenCage. The columns returned by OpenCage have been renamed as follows: ",
-                  paste0(duplicated_colnames, sep = "1", collapse = ", "))
-        }
 
         results <-
           dplyr::select(
