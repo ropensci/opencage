d5414223177e45c73cc3aacca4946c4e71ee8aac
charliejhadley <charliejhadley@gmail.com>
2020-06-30 13:02:29 +0100

    updated tests to account for lifecycle::expect_deprecated

--- tests/testthat/test-deprecated.R
+++ tests/testthat/test-deprecated.R
@@ -19,18 +19,16 @@ test_that("opencage_forward/opencage_reverse return what they should.", {
   skip_on_cran()
   skip_if_offline()
 
-  expect_warning(
-    results <- opencage_forward(placename = "Sarzeau")
-  )
+  expect_deprecated(results <- opencage_forward(placename = "Sarzeau"))
+
   expect_is(results, "list")
   expect_is(results[["results"]], "tbl_df")
   expect_is(results[["total_results"]], "integer")
   expect_is(results[["time_stamp"]], "POSIXct")
   expect_equal(length(results), 4)
 
-  expect_warning(
-    results <- opencage_forward(placename = "Islington, London")
-  )
+  expect_deprecated(results <- opencage_forward(placename = "Islington, London"))
+
   expect_is(results, "list")
   expect_is(results[["results"]], "tbl_df")
   expect_is(results[["total_results"]], "integer")
@@ -38,77 +36,76 @@ test_that("opencage_forward/opencage_reverse return what they should.", {
   expect_equal(length(results), 4)
 
   placename <- "Triererstr 15, Weimar 99423, Deutschland"
-  expect_warning(
-    results <- opencage_forward(placename = placename)
-  )
+
+  expect_deprecated(results <- opencage_forward(placename = placename))
+
   expect_is(results, "list")
   expect_is(results[["results"]], "tbl_df")
   expect_is(results[["total_results"]], "integer")
   expect_is(results[["time_stamp"]], "POSIXct")
   expect_equal(length(results), 4)
 
-  expect_warning(
-    results <-
-      opencage_reverse(
-        longitude = 0,
-        latitude = 0,
+  expect_deprecated(results <-
+    opencage_reverse(
+      longitude = 0,
+      latitude = 0,
+      limit = 2,
+      key = Sys.getenv("OPENCAGE_KEY")
+    ))
+  expect_is(results, "list")
+  expect_is(results[["results"]], "tbl_df")
+  expect_is(results[["total_results"]], "integer")
+  expect_is(results[["time_stamp"]], "POSIXct")
+  expect_equal(length(results), 4)
+})
+
+test_that(
+  "opencage_forward/opencage_reverse return what they should
+          with several parameters.",
+  {
+    skip_on_cran()
+    skip_if_offline()
+
+    expect_warning(
+      results <- opencage_forward(
+        placename = "Paris",
+        key = Sys.getenv("OPENCAGE_KEY"),
         limit = 2,
-        key = Sys.getenv("OPENCAGE_KEY")
+        min_confidence = 5,
+        language = "fr",
+        no_annotations = TRUE
       )
-  )
-  expect_is(results, "list")
-  expect_is(results[["results"]], "tbl_df")
-  expect_is(results[["total_results"]], "integer")
-  expect_is(results[["time_stamp"]], "POSIXct")
-  expect_equal(length(results), 4)
-})
+    )
+    expect_is(results, "list")
+    expect_is(results[["results"]], "tbl_df")
+    expect_is(results[["total_results"]], "integer")
+    expect_is(results[["time_stamp"]], "POSIXct")
+    expect_equal(length(results), 4)
+    expect_equal(sum(grepl("annotation",
+                           names(results[["results"]]))), 0)
+    expect_true(dplyr::between(nrow(results[["results"]]), 1, 2))
 
-test_that("opencage_forward/opencage_reverse return what they should
-          with several parameters.", {
-  skip_on_cran()
-  skip_if_offline()
-
-  expect_warning(
-    results <- opencage_forward(
-      placename = "Paris",
-      key = Sys.getenv("OPENCAGE_KEY"),
-      limit = 2,
-      min_confidence = 5,
-      language = "fr",
-      no_annotations = TRUE)
-  )
-  expect_is(results, "list")
-  expect_is(results[["results"]], "tbl_df")
-  expect_is(results[["total_results"]], "integer")
-  expect_is(results[["time_stamp"]], "POSIXct")
-  expect_equal(length(results), 4)
-  expect_equal(sum(grepl(
-    "annotation",
-    names(results[["results"]])
-  )), 0)
-  expect_true(dplyr::between(nrow(results[["results"]]), 1, 2))
-
-  expect_warning(
-    results <- opencage_reverse(
-      latitude = 44,
-      longitude = 44,
-      key = Sys.getenv("OPENCAGE_KEY"),
-      limit = 2,
-      min_confidence = 5,
-      language = "pt-BR",
-      no_annotations = TRUE)
-  )
-  expect_is(results, "list")
-  expect_is(results[["results"]], "tbl_df")
-  expect_is(results[["total_results"]], "integer")
-  expect_is(results[["time_stamp"]], "POSIXct")
-  expect_equal(length(results), 4)
-  expect_equal(sum(grepl(
-    "annotation",
-    names(results[["results"]])
-  )), 0)
-  expect_true(dplyr::between(nrow(results[["results"]]), 1, 2))
-})
+    expect_warning(
+      results <- opencage_reverse(
+        latitude = 44,
+        longitude = 44,
+        key = Sys.getenv("OPENCAGE_KEY"),
+        limit = 2,
+        min_confidence = 5,
+        language = "pt-BR",
+        no_annotations = TRUE
+      )
+    )
+    expect_is(results, "list")
+    expect_is(results[["results"]], "tbl_df")
+    expect_is(results[["total_results"]], "integer")
+    expect_is(results[["time_stamp"]], "POSIXct")
+    expect_equal(length(results), 4)
+    expect_equal(sum(grepl("annotation",
+                           names(results[["results"]]))), 0)
+    expect_true(dplyr::between(nrow(results[["results"]]), 1, 2))
+  }
+)
 
 test_that("opencage_forward deals well with results being NULL", {
   skip_on_cran()
@@ -121,7 +118,8 @@ test_that("opencage_forward deals well with results being NULL", {
       limit = 2,
       min_confidence = 5,
       language = "pt-BR",
-      no_annotations = TRUE)
+      no_annotations = TRUE
+    )
   )
   expect_is(results, "list")
   expect_null(results[["results"]])
@@ -133,40 +131,39 @@ test_that("the bounds argument is well taken into account", {
   skip_on_cran()
   skip_if_offline()
 
-  expect_warning(
-    results1 <- opencage_forward(
-      placename = "Berlin",
-      key = Sys.getenv("OPENCAGE_KEY")
-  ))
-  expect_warning(
-    results2 <- opencage_forward(
-      placename = "Berlin",
-      bounds = c(-90, 38, 0, 45),
-      key = Sys.getenv("OPENCAGE_KEY")
+  expect_warning(results1 <- opencage_forward(placename = "Berlin",
+                                              key = Sys.getenv("OPENCAGE_KEY")))
+  expect_warning(results2 <- opencage_forward(
+    placename = "Berlin",
+    bounds = c(-90, 38, 0, 45),
+    key = Sys.getenv("OPENCAGE_KEY")
   ))
   expect_true(!("Germany" %in% results2$results$components.country))
   expect_true("Germany" %in% results1$results$components.country)
 })
 
 test_that("Errors with multiple inputs", {
-  expect_error(opencage_forward(c("Hamburg", "Los Angeles")),
-    "`opencage_forward` is not vectorised; use `oc_forward` instead.")
-  expect_error(opencage_reverse(c(5, 20), c(6, 21)),
-    "`opencage_reverse` is not vectorised, use `oc_reverse` instead.")
+  expect_error(
+    opencage_forward(c("Hamburg", "Los Angeles")),
+    "`opencage_forward` is not vectorised; use `oc_forward` instead."
+  )
+  expect_error(
+    opencage_reverse(c(5, 20), c(6, 21)),
+    "`opencage_reverse` is not vectorised, use `oc_reverse` instead."
+  )
 })
 
 
 # Test opencage_key -------------------------------------------------------
 
-test_that("`opencage_key()` returns NULL if OPENCAGE_KEY envvar not found", {
-  withr::local_envvar(c("OPENCAGE_KEY" = ""))
-  expect_null(expect_warning(opencage_key()))
-})
+test_that("`opencage_key()` returns NULL if OPENCAGE_KEY envvar not found",
+          {
+            withr::local_envvar(c("OPENCAGE_KEY" = ""))
+            expect_null(expect_warning(opencage_key()))
+          })
 
 test_that("`opencage_key(quiet = FALSE)` messages", {
   withr::local_envvar(c("OPENCAGE_KEY" = "fakekey"))
-  expect_message(
-    object = expect_warning(opencage_key(quiet = FALSE)),
-    regexp = "Using OpenCage API Key from envvar OPENCAGE_KEY"
-  )
+  expect_message(object = expect_warning(opencage_key(quiet = FALSE)),
+                 regexp = "Using OpenCage API Key from envvar OPENCAGE_KEY")
 })
