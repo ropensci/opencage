1a0bab0f7fa30584aa51ffb800ad240c07841036
Daniel Possenriede <possenriede@gmail.com>
2022-05-03 21:57:23 +0200

    testthat3e adjustments
    - `expect_is()` is deprecated
    -  test lifecycle::expect_deprecated only once, because testthat3e does not ignore messages anymore https://testthat.r-lib.org/articles/third-edition.html#messages

--- tests/testthat/test-deprecated.R
+++ tests/testthat/test-deprecated.R
@@ -1,42 +1,42 @@
 ## Test deprecated opencage_forward ##
 
 test_that("opencage_forward/opencage_reverse return what they should.", {
-  withr::local_envvar(c("OPENCAGE_KEY" = key_200))
   lifecycle::expect_deprecated(
-    results <- opencage_forward(placename = "Sarzeau")
+    results <- opencage_forward(placename = "Sarzeau", key = key_200)
   )
-  expect_is(results, "list")
-  expect_is(results[["results"]], "tbl_df")
-  expect_is(results[["total_results"]], "integer")
-  expect_is(results[["time_stamp"]], "POSIXct")
+  expect_type(results, "list")
+  expect_s3_class(results[["results"]], "tbl_df")
+  expect_type(results[["total_results"]], "integer")
+  expect_s3_class(results[["time_stamp"]], "POSIXct")
   expect_equal(length(results), 4)
 
   lifecycle::expect_deprecated(
-    results <- opencage_reverse(longitude = 0, latitude = 0)
+    results <- opencage_reverse(longitude = 0, latitude = 0, key = key_200)
   )
-  expect_is(results, "list")
-  expect_is(results[["results"]], "tbl_df")
-  expect_is(results[["total_results"]], "integer")
-  expect_is(results[["time_stamp"]], "POSIXct")
+  expect_type(results, "list")
+  expect_s3_class(results[["results"]], "tbl_df")
+  expect_type(results[["total_results"]], "integer")
+  expect_s3_class(results[["time_stamp"]], "POSIXct")
   expect_equal(length(results), 4)
 })
 
 test_that("opencage_forward/opencage_reverse return what they should
           with several parameters.", {
   withr::local_envvar(c("OPENCAGE_KEY" = key_200))
+  withr::local_options(lifecycle_verbosity = "quiet")
 
-  lifecycle::expect_deprecated(
-    results <- opencage_forward(
-      placename = "Paris",
-      limit = 2,
-      min_confidence = 5,
-      language = "fr",
-      no_annotations = TRUE)
+  results <- opencage_forward(
+    placename = "Paris",
+    limit = 2,
+    min_confidence = 5,
+    language = "fr",
+    no_annotations = TRUE
   )
-  expect_is(results, "list")
-  expect_is(results[["results"]], "tbl_df")
-  expect_is(results[["total_results"]], "integer")
-  expect_is(results[["time_stamp"]], "POSIXct")
+
+  expect_type(results, "list")
+  expect_s3_class(results[["results"]], "tbl_df")
+  expect_type(results[["total_results"]], "integer")
+  expect_s3_class(results[["time_stamp"]], "POSIXct")
   expect_equal(length(results), 4)
   expect_equal(sum(grepl(
     "annotation",
@@ -44,62 +44,63 @@ test_that("opencage_forward/opencage_reverse return what they should
   )), 0)
   expect_true(dplyr::between(nrow(results[["results"]]), 1, 2))
 
-  lifecycle::expect_deprecated(
-    results <- opencage_reverse(
-      latitude = 44,
-      longitude = 44,
-      min_confidence = 5,
-      language = "pt-BR",
-      no_annotations = TRUE)
+  results <- opencage_reverse(
+    latitude = 44,
+    longitude = 44,
+    min_confidence = 5,
+    language = "pt-BR",
+    no_annotations = TRUE
   )
-  expect_is(results, "list")
-  expect_is(results[["results"]], "tbl_df")
-  expect_is(results[["total_results"]], "integer")
-  expect_is(results[["time_stamp"]], "POSIXct")
+
+  expect_type(results, "list")
+  expect_s3_class(results[["results"]], "tbl_df")
+  expect_type(results[["total_results"]], "integer")
+  expect_s3_class(results[["time_stamp"]], "POSIXct")
   expect_equal(length(results), 4)
   expect_equal(sum(grepl(
     "annotation",
     names(results[["results"]])
   )), 0)
-  expect_true(dplyr::between(nrow(results[["results"]]), 1, 2))
 })
 
 test_that("opencage_forward deals well with results being NULL", {
   skip_if_no_key()
   skip_if_oc_offline()
+  withr::local_options(lifecycle_verbosity = "quiet")
 
   # the query NOWHERE-INTERESTING will return a valid response with 0 results
   # https://opencagedata.com/api#no-results
-  lifecycle::expect_deprecated(
-    results <- opencage_forward(
-      placename = "NOWHERE-INTERESTING",
-      key = Sys.getenv("OPENCAGE_KEY"),
-      limit = 2,
-      min_confidence = 5,
-      language = "pt-BR",
-      no_annotations = TRUE)
+  results <- opencage_forward(
+    placename = "NOWHERE-INTERESTING",
+    key = Sys.getenv("OPENCAGE_KEY"),
+    limit = 2,
+    min_confidence = 5,
+    language = "pt-BR",
+    no_annotations = TRUE
   )
-  expect_is(results, "list")
+
+  expect_type(results, "list")
   expect_null(results[["results"]])
-  expect_is(results[["total_results"]], "integer")
-  expect_is(results[["time_stamp"]], "POSIXct")
+  expect_type(results[["total_results"]], "integer")
+  expect_s3_class(results[["time_stamp"]], "POSIXct")
 })
 
 test_that("the bounds argument is well taken into account", {
   skip_if_no_key()
   skip_if_oc_offline()
+  withr::local_options(lifecycle_verbosity = "quiet")
 
-  lifecycle::expect_deprecated(
-    results1 <- opencage_forward(
-      placename = "Berlin",
-      key = Sys.getenv("OPENCAGE_KEY")
-  ))
-  lifecycle::expect_deprecated(
-    results2 <- opencage_forward(
-      placename = "Berlin",
-      bounds = c(-90, 38, 0, 45),
-      key = Sys.getenv("OPENCAGE_KEY")
-  ))
+  results1 <- opencage_forward(
+    placename = "Berlin",
+    key = Sys.getenv("OPENCAGE_KEY")
+  )
+
+  results2 <- opencage_forward(
+    placename = "Berlin",
+    bounds = c(-90, 38, 0, 45),
+    key = Sys.getenv("OPENCAGE_KEY")
+  )
+
   expect_true(!("Germany" %in% results2$results$components.country))
   expect_true("Germany" %in% results1$results$components.country)
 })
@@ -118,13 +119,17 @@ test_that("Errors with multiple inputs", {
 
 test_that("`opencage_key()` returns NULL if OPENCAGE_KEY envvar not found", {
   withr::local_envvar(c("OPENCAGE_KEY" = ""))
-  expect_null(lifecycle::expect_deprecated(opencage_key()))
+  withr::local_options(lifecycle_verbosity = "quiet")
+
+  expect_null(opencage_key())
 })
 
 test_that("`opencage_key(quiet = FALSE)` messages", {
   withr::local_envvar(c("OPENCAGE_KEY" = "fakekey"))
+  withr::local_options(lifecycle_verbosity = "quiet")
+
   expect_message(
-    object = lifecycle::expect_deprecated(opencage_key(quiet = FALSE)),
+    object = opencage_key(quiet = FALSE),
     regexp = "Using OpenCage API Key from envvar OPENCAGE_KEY"
   )
 })
