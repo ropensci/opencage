3a774fa5821cdbd9edbc1f26218f198aa9962842
Daniel Possenriede <possenriede@gmail.com>
2022-05-12 22:24:46 +0200

    simplify package docs
    roxygen fetches info from DESCRIPTION

--- R/opencage-package.R
+++ R/opencage-package.R
@@ -1,11 +1,4 @@
-#' opencage: Geocode with the OpenCage API.
-#'
-#' Geocode with the OpenCage API, either from place name to longitude and
-#' latitude (forward geocoding) or from longitude and latitude to the name and
-#' address of a location (reverse geocoding), see <https://opencagedata.com/>.
-#'
-#' @docType package
-#' @name opencage
+#' @keywords internal
 #' @importFrom rlang .data
 "_PACKAGE"
 
