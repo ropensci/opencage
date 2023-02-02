4e5ae63f5ed4c6fc5b8ad336a9dc0fc9ab73b7f6
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-02-02 15:53:23 +0100

    refactor: replace {crul} with {httr2}

--- R/oc_api_ok.R
+++ R/oc_api_ok.R
@@ -10,5 +10,10 @@
 #' @keywords internal
 
 oc_api_ok <- function(url = "https://api.opencagedata.com") {
-  crul::ok(url, useragent = "https://github.com/ropensci/opencage")
+  resp <- httr2::request("https://api.opencagedata.com") %>%
+    httr2::req_method("HEAD") %>%
+    httr2::req_user_agent("https://github.com/ropensci/opencage") %>%
+    httr2::req_perform()
+
+  !httr2::resp_is_error(resp)
 }
