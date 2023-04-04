23cab1d8608d10fb01e8f122974f4eb8207f14b3
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-04-04 15:40:40 +0200

    Update tests/testthat/test-oc_clear_cache.R
    Co-authored-by: Daniel Possenriede <possenriede@gmail.com>

--- tests/testthat/test-oc_clear_cache.R
+++ tests/testthat/test-oc_clear_cache.R
@@ -2,9 +2,6 @@ test_that("oc_clear_cache clears cache", {
   skip_on_cran()
   skip_if_offline("httpbin.org")
 
-  # until a memoise >v.1.1 is released, we need to run oc_get_memoise() twice to
-  # have it really cache results
-  # https://github.com/ropensci/opencage/pull/87#issuecomment-573573183
   replicate(2, oc_get_memoise())
   expect_true(memoise::has_cache(oc_get_memoise)())
   oc_clear_cache()
