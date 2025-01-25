59117453d7d9ff5638fae2ecf715b5a27e594dab
Daniel Possenriede <possenriede@gmail.com>
2025-01-25 20:56:18 +0100

    fix lintr issues

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -8,8 +8,8 @@ library(knitr)
 stopifnot("no OPENCAGE_KEY envvar present" = nzchar(Sys.getenv("OPENCAGE_KEY")))
 
 # write data frames as markdown tables via `knitr::kable()`
-knit_print.data.frame = function(x, ...) {
-  res = paste0(c(kable(x, output = FALSE)), collapse = "\n")
+knit_print.data.frame <- function(x, ...) { # nolint
+  res <- paste0(c(kable(x, output = FALSE)), collapse = "\n")
   asis_output(res)
 }
 # register the method
@@ -20,7 +20,7 @@ knit("vignettes/customise_query.Rmd.src", "vignettes/customise_query.Rmd")
 
 # reset print method for output_options vignette,
 # because kable() doesn't print list columns well
-knit_print.data.frame = function(x, ...) {
+knit_print.data.frame <- function(x, ...) { # nolint
   print(x)
 }
 # register the method
