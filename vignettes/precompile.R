628f08e656dcaa1237d1f9f7d6428d6e4d19cf8f
Daniel Possenriede <possenriede@gmail.com>
2025-01-25 20:08:31 +0100

    use knitr::kable() output for dataframes in opencage and customise vignette

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -2,6 +2,24 @@
 
 library(knitr) # also load the current version of the package
 stopifnot("no OPENCAGE_KEY envvar present" = nzchar(Sys.getenv("OPENCAGE_KEY")))
+
+# write data frames as markdown tables via `knitr::kable()`
+knit_print.data.frame = function(x, ...) {
+  res = paste0(c(kable(x, output = FALSE)), collapse = "\n")
+  asis_output(res)
+}
+# register the method
+registerS3method("knit_print", "data.frame", knit_print.data.frame)
+
 knit("vignettes/opencage.Rmd.src", "vignettes/opencage.Rmd")
 knit("vignettes/customise_query.Rmd.src", "vignettes/customise_query.Rmd")
+
+# reset print method for output_options vignette,
+# because kable() doesn't print list columns well
+knit_print.data.frame = function(x, ...) {
+  print(x)
+}
+# register the method
+registerS3method("knit_print", "data.frame", knit_print.data.frame)
+
 knit("vignettes/output_options.Rmd.src", "vignettes/output_options.Rmd")
